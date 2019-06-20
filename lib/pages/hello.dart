be13358b1a55e5cf6485e4dace83b61b4bcc9ea4
jingle1267 <jingle1267@163.com>
2019-06-20 18:10:35 +0800

    [ADD] Add hello world.

---
+++ lib/pages/hello.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Hello extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Center(
+        child: Text("hello world"),
+      ),
+    );
+  }
+}
